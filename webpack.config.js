dabfcb458c9ada6d3658ff02f003c138adb11171
Federico Tomassetti <federico@tomassetti.me>
2019-09-01 09:12:57 +0200

    serve HTML file rendering Monaco

---
+++ webpack.config.js
@@ -0,0 +1,29 @@
+module.exports = {
+    entry: './src/main/javascript/index.js',
+    output: {
+        filename: 'main.js',
+    },
+    module: {
+        rules: [{
+            test: /\.tsx?$/,
+            use: 'ts-loader',
+            exclude: /node_modules/
+        }]
+    },
+    resolve: {
+        modules: ['node_modules'],
+        extensions: [ '.tsx', '.ts', '.js' ]
+    },
+    mode: 'production',
+    node: {
+        fs: 'empty',
+        global: true,
+        crypto: 'empty',
+        tls: 'empty',
+        net: 'empty',
+        process: true,
+        module: false,
+        clearImmediate: false,
+        setImmediate: false
+    }
+}
