70020dd5f98034a3adb5a86652d1e1dd25b50fc9
Federico Tomassetti <federico@tomassetti.me>
2019-09-03 17:15:37 +0200

    add functions to process errors

--- src/main/typescript/ParserFacade.ts
+++ src/main/typescript/ParserFacade.ts
@@ -1,12 +1,48 @@
 /// <reference path="../../../node_modules/monaco-editor/monaco.d.ts" />
 
-import {CommonTokenStream, InputStream, Token, error} from '../../../node_modules/antlr4/index.js'
+import {CommonTokenStream, InputStream, Token, error, Parser} from '../../../node_modules/antlr4/index.js'
+import {DefaultErrorStrategy} from '../../../node_modules/antlr4/error/ErrorStrategy.js'
 import {CalcLexer} from "../../main-generated/javascript/CalcLexer.js"
 import {CalcParser} from "../../main-generated/javascript/CalcParser.js"
 
-class MyErrorListener extends error.ErrorListener {
+class ConsoleErrorListener extends error.ErrorListener {
     syntaxError(recognizer, offendingSymbol, line, column, msg, e) {
         console.log("ERROR " + msg);
+    }
+}
+
+export class Error {
+    startLine: number;
+    endLine: number;
+    startCol: number;
+    endCol: number;
+    message: string;
+
+    constructor(startLine: number, endLine: number, startCol: number, endCol: number, message: string) {
+        this.startLine = startLine;
+        this.endLine = endLine;
+        this.startCol = startCol;
+        this.endCol = endCol;
+        this.message = message;
+    }
+
+}
+
+class CollectorErrorListener extends error.ErrorListener {
+
+    private errors : Error[] = []
+
+    constructor(errors: Error[]) {
+        super()
+        this.errors = errors
+    }
+
+    syntaxError(recognizer, offendingSymbol, line, column, msg, e) {
+        var endColumn = column + 1;
+        if (offendingSymbol._text !== null) {
+            endColumn = column + offendingSymbol._text.length;
+        }
+        this.errors.push(new Error(line, line, column, endColumn, msg));
     }
 
 }
@@ -44,13 +80,67 @@ function parseTree(input) {
 export function parseTreeStr(input) {
     const lexer = createLexer(input);
     lexer.removeErrorListeners();
-    lexer.addErrorListener(new MyErrorListener());
+    lexer.addErrorListener(new ConsoleErrorListener());
 
     const parser = createParserFromLexer(lexer);
     parser.removeErrorListeners();
-    parser.addErrorListener(new MyErrorListener());
+    parser.addErrorListener(new ConsoleErrorListener());
 
     const tree = parser.compilationUnit();
 
     return tree.toStringTree(parser.ruleNames);
+}
+
+class CalcErrorStrategy extends DefaultErrorStrategy {
+
+     reportUnwantedToken(recognizer: Parser) {
+         return super.reportUnwantedToken(recognizer);
+     }
+
+    singleTokenDeletion(recognizer: Parser) {
+        var nextTokenType = recognizer.getTokenStream().LA(2);
+        if (recognizer.getTokenStream().LA(1) == CalcParser.NL) {
+            return null;
+        }
+        var expecting = this.getExpectedTokens(recognizer);
+        if (expecting.contains(nextTokenType)) {
+            this.reportUnwantedToken(recognizer);
+            // print("recoverFromMismatchedToken deleting " \
+            // + str(recognizer.getTokenStream().LT(1)) \
+            // + " since " + str(recognizer.getTokenStream().LT(2)) \
+            // + " is what we want", file=sys.stderr)
+            recognizer.consume(); // simply delete extra token
+            // we want to return the token we're actually matching
+            var matchedSymbol = recognizer.getCurrentToken();
+            this.reportMatch(recognizer); // we know current token is correct
+            return matchedSymbol;
+        } else {
+            return null;
+        }
+    }
+    getExpectedTokens = function(recognizer) {
+        return recognizer.getExpectedTokens();
+    };
+
+    reportMatch = function(recognizer) {
+        this.endErrorCondition(recognizer);
+    };
+
+
+}
+
+export function validate(input) : Error[] {
+    let errors : Error[] = []
+
+    const lexer = createLexer(input);
+    lexer.removeErrorListeners();
+    lexer.addErrorListener(new ConsoleErrorListener());
+
+    const parser = createParserFromLexer(lexer);
+    parser.removeErrorListeners();
+    parser.addErrorListener(new CollectorErrorListener(errors));
+    parser._errHandler = new CalcErrorStrategy();
+
+    const tree = parser.compilationUnit();
+    return errors;
 }
